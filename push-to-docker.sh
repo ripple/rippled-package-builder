58339c9a4173cd41cd28415792933eddc1a59578
Steven Zeiler <zeiler.steven@gmail.com>
2015-10-08 15:30:23 -0700

    [TASK] Use docker hub instead of quay.io

--- push-to-docker.sh
+++ push-to-docker.sh
@@ -1,5 +1,5 @@
-docker tag rippled-build-bot quay.io/stevenzeiler/rippled-build-bot:$CIRCLE_SHA
-docker tag rippled-build-bot quay.io/stevenzeiler/rippled-build-bot:$1
-docker login --email=$DOCKER_EMAIL --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD quay.io
-docker push quay.io/stevenzeiler/rippled-build-bot:$CIRCLE_SHA
-docker push quay.io/stevenzeiler/rippled-build-bot:$1
+docker tag rippled-build-bot stevenzeiler/rippled-build-bot:$CIRCLE_SHA
+docker tag rippled-build-bot stevenzeiler/rippled-build-bot:$1
+docker login --email=$DOCKER_EMAIL --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
+docker push stevenzeiler/rippled-build-bot:$CIRCLE_SHA
+docker push stevenzeiler/rippled-build-bot:$1
