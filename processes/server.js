673c1cb3cc891738a44989027dcd2fa26e1038e5
Steven Zeiler <stevenzeiler@Stevens-MacBook-Air.local>
2015-10-14 12:40:56 -0700

    [FEATURE] List RPM build files webpage
    [FIX] Remove invalid test
    
    [FOLD] Remove test command

--- processes/server.js
+++ processes/server.js
@@ -1,10 +1,16 @@
 var path           = require('path')
 var BridgesExpress = require('bridges-express')
 var port           = process.env.PORT || 5000
+var express        = require('express')
 
 module.exports = function(models) {
+  var app = express()
+
+  app.set('view engine', 'ejs');
+  app.use('/', express.static('static'))
 
   var server = new BridgesExpress({
+    app: app,
     directory: path.join(__dirname, '..'),
     controllers: {
       inject: [models]
