04c6b9c1984a0aaae4b9e71b1415b4a5f298ef30
wilsonianb <bwilson@ripple.com>
2015-10-21 12:47:47 -0700

    [FEATURE] Check rpm's md5sum when deploying and testing DEC-658

--- processes/sqs_deployed_worker.js
+++ processes/sqs_deployed_worker.js
@@ -3,20 +3,33 @@ import Events from '../lib/events'
 
 const QUEUE_URL = 'https://sqs.us-west-2.amazonaws.com/356003847803/rippled-rpm-deployed-staging'
 
+class RPMFile {
+  constructor(message) {
+    const rpm_message = JSON.parse(message.Body)
+
+    return {
+      commit_hash: rpm_message.commit_hash,
+      md5sum: rpm_message.md5sum,
+      rippled_version: rpm_message.rippled_version,
+      yum_repo: rpm_message.yum_repo
+    }
+  }
+}
+
 class Worker extends SQSWorker {
 
   onMessage(message, done) {
 
-    var yumRepo
+    var rpmFile
 
     try {
-      yumRepo = JSON.parse(message.Body).repo
+      rpmFile = new RPMFile(message)
     } catch(error) {
       console.log("error", error)
       done()
     }
 
-    Events.emit('s3:rpm:deployed', yumRepo)
+    Events.emit('sqs:rpm:deployed', rpmFile)
 
     done()
   }
--- rpm-tester/test_rpm.sh
+++ rpm-tester/test_rpm.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
-yum install -y --enablerepo=$YUM_REPO rippled
 
-yumdownloader --source --enablerepo=$YUM_REPO rippled
-rpm -i rippled-*.src.rpm
-tar -zxf ~/rpmbuild/SOURCES/rippled.tar.gz -C ./
-cd rippled
-npm install
-mkdir build
-ln -s /opt/ripple/bin/rippled build/rippled
+# Check rpm's md5sum
+yumdownloader --enablerepo=$YUM_REPO rippled
+REPO_MD5SUM=`rpm -Kv *.x86_64.rpm | grep 'MD5 digest' | grep -oP '\(\K[^)]+'`
+if [ "$REPO_MD5SUM" != "$MD5SUM" ]; then
+  ERROR="md5sum mismatch ($REPO_MD5SUM)"
+else
+  yum install -y --enablerepo=$YUM_REPO rippled
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib /opt/ripple/bin/rippled --unittest
+  yumdownloader --source --enablerepo=$YUM_REPO rippled
+  rpm -i rippled-*.src.rpm
+  tar -zxf ~/rpmbuild/SOURCES/rippled.tar.gz -C ./
+  cd rippled
+  npm install
+  mkdir build
+  ln -s /opt/ripple/bin/rippled build/rippled
 
-rc=$?; if [[ $rc != 0 ]]; then
-  echo "rippled --unittest failed"
-  exit $rc
+  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib /opt/ripple/bin/rippled --unittest
+
+  rc=$?; if [[ $rc != 0 ]]; then
+    ERROR="rippled --unittest failed"
+  else
+    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib npm test
+
+    rc=$?; if [[ $rc != 0 ]]; then
+      ERROR="npm test failed"
+    fi
+  fi
 fi
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib npm test
-
-rc=$?; if [[ $rc != 0 ]]; then
-  echo "npm test failed"
-  exit $rc
+if [ -n "$ERROR" ]; then
+  echo $ERROR
+  aws sqs send-message --queue-url https://sqs.us-west-2.amazonaws.com/356003847803/rippled-rpm-failed --message-body "{\"stage\":\"rpm-tester\", \"error\":\"$ERROR\", \"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\"}" --region $AWS_REGION
+  exit 1
 fi
 
-echo "tests passed"
-
-aws sqs send-message --queue-url https://sqs.us-west-2.amazonaws.com/356003847803/rippled-rpm-tested --message-body '{"results":"passed"}' --region us-west-2
+aws sqs send-message --queue-url https://sqs.us-west-2.amazonaws.com/356003847803/rippled-rpm-tested --message-body "{\"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\"}" --region us-west-2
