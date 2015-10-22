f620b421a3abe738b5f9792ec016db0e29e4b717
wilsonianb <bwilson@ripple.com>
2015-10-22 14:18:29 -0700

    [TASK] Improve error handling in container scripts

--- rpm-deployer/deploy_rpm.sh
+++ rpm-deployer/deploy_rpm.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
+
+function error {
+  echo $1
+  aws sqs send-message --queue-url $SQS_QUEUE_FAILED --message-body "{\"stage\":\"rpm-deployer\", \"error\":\"$1\", \"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
+  exit 1
+}
+
 cd /home/docker/rpm-deployer/ansible
 
 mkdir rpms
+
 aws s3 cp s3://$S3_BUCKET/$S3_KEY . --region $S3_REGION
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error downloading rpm from s3://$S3_BUCKET/$S3_KEY"
+fi
 
 tar -zxvf $S3_KEY -C rpms
 
 # Check rpm's md5sum
 S3_MD5SUM=`rpm -Kv rpms/*.x86_64.rpm | grep 'MD5 digest' | grep -oP '\(\K[^)]+'`
 if [ "$S3_MD5SUM" != "$MD5SUM" ]; then
-  ERROR="md5sum mismatch ($S3_MD5SUM)"
-else
-  ansible-playbook -vvv -i hosts staging.yml
-
-  rc=$?; if [[ $rc != 0 ]]; then
-    ERROR="ansible deployment failed"
-  fi
+  error "md5sum mismatch ($S3_MD5SUM)"
 fi
 
-if [ -n "$ERROR" ]; then
-  echo $ERROR
-  aws sqs send-message --queue-url $SQS_QUEUE_FAILED --message-body "{\"stage\":\"rpm-deployer\", \"error\":\"$ERROR\", \"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
-  exit 1
+ansible-playbook -vvv -i hosts staging.yml
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error deploying to $YUM_REPO with ansible"
 fi
 
 aws sqs send-message --queue-url $SQS_QUEUE_DEPLOYED --message-body "{\"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error sending message to $SQS_QUEUE_DEPLOYED"
+fi
--- rpm-tester/test_rpm.sh
+++ rpm-tester/test_rpm.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
+function error {
+  echo $1
+  aws sqs send-message --queue-url $SQS_QUEUE_FAILED --message-body "{\"stage\":\"rpm-tester\", \"error\":\"$1\", \"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
+  exit 1
+}
+
 # Check rpm's md5sum
 yumdownloader --enablerepo=$YUM_REPO rippled
 REPO_MD5SUM=`rpm -Kv *.x86_64.rpm | grep 'MD5 digest' | grep -oP '\(\K[^)]+'`
 if [ "$REPO_MD5SUM" != "$MD5SUM" ]; then
-  ERROR="md5sum mismatch ($REPO_MD5SUM)"
-else
-  yum install -y --enablerepo=$YUM_REPO rippled
-
-  yumdownloader --source --enablerepo=$YUM_REPO rippled
-  rpm -i rippled-*.src.rpm
-  tar -zxf ~/rpmbuild/SOURCES/rippled.tar.gz -C ./
-  cd rippled
-  npm install
-  mkdir build
-  ln -s /opt/ripple/bin/rippled build/rippled
-
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib /opt/ripple/bin/rippled --unittest
-
-  rc=$?; if [[ $rc != 0 ]]; then
-    ERROR="rippled --unittest failed"
-  else
-    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib npm test
-
-    rc=$?; if [[ $rc != 0 ]]; then
-      ERROR="npm test failed"
-    fi
-  fi
+  error "md5sum mismatch ($REPO_MD5SUM)"
 fi
 
-if [ -n "$ERROR" ]; then
-  echo $ERROR
-  aws sqs send-message --queue-url $SQS_QUEUE_FAILED --message-body "{\"stage\":\"rpm-tester\", \"error\":\"$ERROR\", \"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
-  exit 1
+yum install -y --enablerepo=$YUM_REPO rippled
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error downloading rippled rpm from $YUM_REPO"
 fi
 
-aws sqs send-message --queue-url $SQS_QUEUE_TESTED --message-body "{\"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region SQS_REGION
+yumdownloader --source --enablerepo=$YUM_REPO rippled
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error downloading rippled source rpm from $YUM_REPO"
+fi
+
+rpm -i rippled-*.src.rpm
+tar -zxf ~/rpmbuild/SOURCES/rippled.tar.gz -C ./
+cd rippled
+npm install
+mkdir build
+ln -s /opt/ripple/bin/rippled build/rippled
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib /opt/ripple/bin/rippled --unittest
+rc=$?; if [[ $rc != 0 ]]; then
+  error "rippled --unittest failed"
+fi
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ripple/openssl/lib:/opt/ripple/boost/lib npm test
+rc=$?; if [[ $rc != 0 ]]; then
+  error "npm test failed"
+fi
+
+aws sqs send-message --queue-url $SQS_QUEUE_TESTED --message-body "{\"yum_repo\":\"$YUM_REPO\", \"commit_hash\":\"$COMMIT_HASH\", \"md5sum\":\"$MD5SUM\", \"rippled_version\":\"$RIPPLED_VERSION\", \"commit_signer\":\"$COMMIT_SIGNER\"}" --region $SQS_REGION
+rc=$?; if [[ $rc != 0 ]]; then
+  error "error sending message to $SQS_QUEUE_TESTED"
+fi
