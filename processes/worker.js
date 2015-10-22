04a49e472fb866fba8b41114cbb8575c1ae3ce75
wilsonianb <bwilson@ripple.com>
2015-10-22 16:12:08 -0700

    [TASK] Trigger rpm builder from SQS message

--- processes/worker.js
+++ processes/worker.js
@@ -1,27 +1,12 @@
 import Events from '../lib/events'
-import {RPMFromCommit, DeployRPMToStaging, TestStagingRPM} from '../lib/docker'
+import {BuildRPMFromCommit, DeployRPMToStaging, TestStagingRPM} from '../lib/docker'
 import {postToSlack} from '../lib/slack'
 
 module.exports = function() {
 
-  Events.on('push:develop', function(message) {
-    // trigger build for "nightly" yum repostory
-  })
-
-  Events.on('push:release', function(message) {
-    // trigger build for "unstable" yum repostory
-  })
-
-  Events.on('push:master', function(message) {
-    // trigger build for "stable" yum repostory
-    const commitHash = message.after
-
-    if (commitHash) {
-      RPMFromCommit(commitHash, process.env.S3_BUCKET_STABLE, 'ripple-stable')
-      postToSlack(`I am now building a rippled RPM from the master branch commit ${commitHash}`)
-    } else {
-      console.error('no commit hash')
-    }
+  Events.on('sqs:rippled:commit:pushed', function(message) {
+    BuildRPMFromCommit(message.commit_hash, message.s3_bucket, message.yum_repo)
+    postToSlack(`I am now building a rippled RPM from the ${message.branch} branch commit ${message.commit_hash}`)
   })
 
   Events.on('sqs:rpm:uploaded', function(message) {
