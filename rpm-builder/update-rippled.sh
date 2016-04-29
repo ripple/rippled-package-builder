e3fa8e35794e6fec3f663d8e24099a7cfebb10d5
wilsonianb <bwilson@ripple.com>
2016-04-29 13:43:48 -0700

    Get latest ripple.repo before updating rippled

--- rpm-builder/update-rippled.sh
+++ rpm-builder/update-rippled.sh
@@ -2,6 +2,9 @@
 
 RIPPLE_REPO=${RIPPLE_REPO-stable}
 
+# Update ripple.repo file
+rpm -Uvh --replacepkgs https://mirrors.ripple.com/ripple-repo-el7.rpm
+
 yum --disablerepo=* --enablerepo=ripple-$RIPPLE_REPO clean expire-cache
 yum check-update --enablerepo=ripple-$RIPPLE_REPO rippled
 
